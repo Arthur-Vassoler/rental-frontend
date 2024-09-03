15390b7dd0775e77b9adc48796679e7d819d8f16
Arthur-Vassoler <a.h.vassoler@gmail.com>
2024-09-03 16:35:05 -0300

    ajustado para formulário e lista funcionarem como esperado

--- src/app/pages/guest/guest.component.ts
+++ src/app/pages/guest/guest.component.ts
@@ -3,18 +3,33 @@ import { FormComponent } from './form/form.component';
 import { DynamicListComponent } from '../../components/dynamic-list/dynamic-list.component';
 import { Observable } from 'rxjs';
 import { GuestService } from '../../services/guest.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-guest',
   standalone: true,
-  imports: [FormComponent, DynamicListComponent],
+  imports: [FormComponent, DynamicListComponent, CommonModule],
   templateUrl: './guest.component.html',
-  styleUrl: './guest.component.scss'
+  styleUrls: ['./guest.component.scss']
 })
 export class GuestComponent {
   items$: Observable<any[]>;
+  isFormVisible = false;
 
   constructor(private dataService: GuestService) {
     this.items$ = this.dataService.getItems();
+  }
+
+  openForm(): void {
+    this.isFormVisible = true;
+  }
+
+  onFormSubmitted(event: any): void {
+    this.isFormVisible = false;
+    this.items$ = this.dataService.getItems();
+  }
+
+  goBackToList(): void {
+    this.isFormVisible = false;
   }
 }
